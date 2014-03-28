1f67bd191101c64088f5f4d06a33b58341464d41
Evgene Bruter <evgene.bruter@hp.com>
2014-03-28 23:45:40 +0300

    HW2: removed dev files from gh-pages

--- project/unit2/walkthroughs/w3/w3.ts
+++
@@ -1,9 +0,0 @@
-// Creates a module
-var auction = angular.module('auction', []);
-
-auction.controller('IndexController', ['$scope', function ($scope) {
-    $scope.model = {
-        message: 'Initial message'
-    };
-
-}]);
