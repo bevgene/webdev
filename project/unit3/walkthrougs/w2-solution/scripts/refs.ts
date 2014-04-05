9878c31ec296f264386dda0dc268a83a835bbd3e
Evgene Bruter <evgene.bruter@hp.com>
2014-04-05 08:26:49 +0300

    Unit3-HW: initial commit.

---
+++ project/unit3/walkthrougs/w2-solution/scripts/refs.ts
@@ -0,0 +1,15 @@
+/*************** Libraries ***************/
+
+/// <reference path="../lib/DefinitelyTyped/jquery/jquery.d.ts" />
+/// <reference path="../lib/DefinitelyTyped/angularjs/angular.d.ts" />
+/// <reference path="../lib/DefinitelyTyped/angularjs/angular-route.d.ts" />
+
+
+/*************** Project Files ***************/
+
+/// <reference path="controller/HomeController.ts" />
+/// <reference path="controller/SearchController.ts" />
+/// <reference path="directive/FooterDirective.ts" />
+/// <reference path="directive/NavbarDirective.ts" />
+/// <reference path="model/ProductModel.ts" />
+/// <reference path="service/ProductService.ts" />
