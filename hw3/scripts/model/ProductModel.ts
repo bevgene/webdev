5204d84384b8fa7650ad46e76cfc5ad8f759d3a1
Evgene Bruter <evgene.bruter@hp.com>
2014-04-05 10:36:17 +0300

    HW3: non-dist version

---
+++ hw3/scripts/model/ProductModel.ts
@@ -0,0 +1,15 @@
+// Implement "POJO" here to keep data of a single Product item.
+/// <reference path='../refs.ts'/>
+
+module auction.model {
+    export class ProductModel {
+        id         :    number;
+        title      :    string;
+        thumb      :    string;
+        description:    string;
+        timeleft   :    number;
+        watchers   :    number;
+        price      :    number;
+        url        :    string;
+    }
+}
